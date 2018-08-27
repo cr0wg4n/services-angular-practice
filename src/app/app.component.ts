88ea5b317128985cad6ce1355cb563de0ea4155e
cr0wg4n <dark.hunter.crow@gmail.com>
2018-08-27 10:43:43 -0400

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService} from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'primera-app';
+  name:String="";
+  age:number;
+ users:string[]=["lucas","zoraya","ramona"];
+
+ constructor(private dataService:DataService){
+  this.dataService.getData().subscribe(data =>{
+    this.posts=data;
+  });
+ }
+ deleteUser(user){
+   for(let i=0;i<this.users.length;i++){
+    if(this.users[i]==user){
+      this.users.splice(i,1);
+    }
+   }
+
+ }
+ addUser(user){
+   this.users.push(user.value);
+   user.value ='';
+   return false;
+ }
+ posts = [];
 }
